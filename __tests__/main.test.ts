fa7b173f9692dd28d0cb5d1398e46e0e82deb112
danielb <daniel.bannert@limango.de>
2020-04-28 22:56:47 +0200

    test pipe2

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,13 +1,23 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import * as core from '@actions/core'
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
+jest.mock('core');
+
+const run = path.join(__dirname, '..', 'lib', 'main.js')
+
+describe('Update template fork', () => {
+  it('should throw a setFailed on empty template var', () => {
+    core.getInput
+      .mockReturnValueOnce(undefined)
+      .mockReturnValueOnce(undefined)
+      .mockReturnValueOnce(undefined)
+
+      const options: cp.ExecSyncOptions = {
+        env: process.env
+      }
+
+      cp.execSync(`node ${run}`, options)
+  })
 })
