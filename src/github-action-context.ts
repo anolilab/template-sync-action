2ea0d3a0728003537de4b15c2c9ebadc2c4675b4
prisis <d.bannert@anolilab.de>
2021-02-22 11:50:40 +0100

    feat: upgraded dependencies feat: upgraded old functions calls style: cs fixes

--- src/github-action-context.ts
+++ src/github-action-context.ts
@@ -1,41 +1,37 @@
-import {readFileSync, existsSync} from 'fs'
-import {IWebhookPayload} from './interfaces'
-import {EOL} from 'os'
+import { readFileSync, existsSync } from "fs";
+import { IWebhookPayload } from "./interfaces";
+import { EOL } from "os";
 
 export class GithubActionContext {
-  payload: IWebhookPayload
+    payload: IWebhookPayload;
 
-  constructor() {
-    this.payload = {}
+    constructor() {
+        this.payload = {};
 
-    if (process.env.GITHUB_EVENT_PATH) {
-      if (existsSync(process.env.GITHUB_EVENT_PATH)) {
-        this.payload = JSON.parse(
-          readFileSync(process.env.GITHUB_EVENT_PATH, {encoding: 'utf8'})
-        )
-      } else {
-        const path = process.env.GITHUB_EVENT_PATH
+        if (process.env.GITHUB_EVENT_PATH) {
+            if (existsSync(process.env.GITHUB_EVENT_PATH)) {
+                this.payload = JSON.parse(readFileSync(process.env.GITHUB_EVENT_PATH, { encoding: "utf8" }));
+            } else {
+                const path = process.env.GITHUB_EVENT_PATH;
 
-        process.stdout.write(`GITHUB_EVENT_PATH ${path} does not exist${EOL}`)
-      }
-    }
-  }
-
-  get repo(): {owner: string; repo: string} {
-    if (process.env.GITHUB_REPOSITORY) {
-      const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
-      return {owner, repo}
+                process.stdout.write(`GITHUB_EVENT_PATH ${path} does not exist${EOL}`);
+            }
+        }
     }
 
-    if (this.payload.repository) {
-      return {
-        owner: this.payload.repository.owner.login,
-        repo: this.payload.repository.name
-      }
-    }
+    get repo(): { owner: string; repo: string } {
+        if (process.env.GITHUB_REPOSITORY) {
+            const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+            return { owner, repo };
+        }
 
-    throw new Error(
-      "context.repo requires a GITHUB_REPOSITORY environment variable like 'owner/repo'"
-    )
-  }
+        if (this.payload.repository) {
+            return {
+                owner: this.payload.repository.owner.login,
+                repo: this.payload.repository.name,
+            };
+        }
+
+        throw new Error("context.repo requires a GITHUB_REPOSITORY environment variable like 'owner/repo'");
+    }
 }
